3d4c9990de101340df2f3b9000d8a240d672c775
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-04-14 19:44:55 +0900

    테스트

--- src/main/java/com/example/firstproject/service/ArticleService.java
+++ src/main/java/com/example/firstproject/service/ArticleService.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -78,5 +79,25 @@ public class ArticleService {
          * 이는 삭제 작업이 성공했음을 나타내는 동시에, 
          * 삭제된 엔티티에 대한 정보를 사용할 수 있게 한다(예를 들어, 로깅이나 추가적인 처리를 위해).
          */
+    }
+
+    @Transactional
+    public List<Article> createArticles(List<ArticleForm> dtos) {
+        // 1. dto 묶음을 엔티티 묶음으로 변환하기
+        List<Article> articleList = dtos.stream()
+                .map(dto -> dto.toEntity())
+                .toList();
+
+        // 2. 엔티티 묶음을 DB에 저장하기
+        articleList.stream()
+                .forEach(article -> articleRepository.save(article));
+
+        // 3. 강제 예외 발생시키기
+        articleRepository.findById(-1L)
+                .orElseThrow(() -> new IllegalArgumentException("결제 실패"));
+
+        // 4. 결과 값 반환하기
+
+        return articleList;
     }
 }
