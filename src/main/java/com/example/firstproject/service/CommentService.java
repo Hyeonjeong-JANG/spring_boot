487cd23701af7605d9e43e6f6db4ebbad4ba74e6
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-04-19 22:43:08 +0900

    댓글 작성하기

--- src/main/java/com/example/firstproject/service/CommentService.java
+++ src/main/java/com/example/firstproject/service/CommentService.java
@@ -1,11 +1,13 @@
 package com.example.firstproject.service;
 
 import com.example.firstproject.dto.CommentDTO;
+import com.example.firstproject.entity.Article;
 import com.example.firstproject.entity.Comment;
 import com.example.firstproject.repository.ArticleRepository;
 import com.example.firstproject.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,5 +32,21 @@ public class CommentService {
 
         // 3. 결과 반환
         return commentRepository.findByArticleId(articleId).stream().map(comment -> CommentDTO.createCommentDTO(comment)).toList();
+    }
+
+    @Transactional
+    public CommentDTO create(Long articleId, CommentDTO dto) {
+        // 1. 게시글 조회 및 예외 발생
+        Article article = articleRepository.findById(articleId)
+                .orElseThrow(() -> new IllegalArgumentException("댓글 생성 실패!" + "대상 게시글이 없습니다."));
+
+        // 2. 댓글 엔티티 생성
+        Comment comment = Comment.createComment(dto, article);
+
+        // 3. 댓글 엔티티를 DB에 저장
+        Comment created = commentRepository.save(comment);
+
+        // 4. DTO로 변환해 반환
+        return CommentDTO.createCommentDTO(created);
     }
 }
